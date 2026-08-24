171f3479a114c3a428c8936148bc235a104c6a03
durgeshkadam-beep <durgesh.kadam@gmail.com>
2026-08-24 15:05:08 +0530

    Refresh V15 service worker cache after boot fix

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "iti-v15-production-20260824-10";
+const CACHE = "iti-v15-production-20260824-11";
 const ASSETS = ["./", "./start.html", "./index.html", "./style.css", "./app.js", "./ai.js", "./cloud.js", "./security-patch.js", "./admission-import.js", "./other-printable-records.js", "./official-plans.js", "./manifest.json", "./icon.svg", "./logo.png"];
 
 self.addEventListener("install", e=>{
