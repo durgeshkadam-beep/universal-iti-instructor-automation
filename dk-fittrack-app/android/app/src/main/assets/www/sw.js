d2cd3354c635a3ccb3d2bd949a9cbeb3e60d64bc
durgeshkadam-beep <durgesh.kadam@gmail.com>
2026-08-27 15:13:50 +0530

    Add DK FitTrack: dk-fittrack-app/android/app/src/main/assets/www/sw.js

---
+++ dk-fittrack-app/android/app/src/main/assets/www/sw.js
@@ -0,0 +1,5 @@
+const CACHE="dk-fittrack-v1";
+const ASSETS=["./","./index.html","./styles.css","./v3.css","./app.js","./v3.js","./sync.js","./firebase-config.js","./cloud.js","./manifest.json","./icons/icon.svg"];
+self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
+self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
+self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
